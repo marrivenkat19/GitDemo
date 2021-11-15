90ac9b0cbaaf336799440603542f96c9a16801cf
Venkateswara Reddy Marri <venkateswarareddy@nbos.vinisoft.com>
2021-11-15 14:55:11 +0530

    5th commit

--- src/main/java/com/example/demo/AddNumbers.java
+++ src/main/java/com/example/demo/AddNumbers.java
@@ -1,11 +1,8 @@
 package com.example.demo;
 
 public class AddNumbers {
- int A = 100; 
-  int B = 200; 
-   int K = A+B;
+ 
    System.out.println(" its pleasure to working mode !");
-   Syststem.out.println(" sum of 2 numbers : " + K);
-   
+      
      
 }
